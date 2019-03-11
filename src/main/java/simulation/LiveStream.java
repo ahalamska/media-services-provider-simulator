058a440a5670f31f202387dedab8c2a418cc12c7
ahalamska <a.halamska@gmail.com>
2019-03-11 13:45:56 +0100

    added foo

--- src/main/java/simulation/LiveStream.java
+++ src/main/java/simulation/LiveStream.java
@@ -1,5 +1,7 @@
 package simulation;
 
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -8,5 +10,7 @@ public class LiveStream extends Product implements Serializable {
     public LiveStream(String name, String description, String releaseDate, int length,  String country, Double rating, Double prise, String licenseNR) {
         super(name, description, releaseDate, length,  country, rating, prise, licenseNR);
     }
+    public void foo(){};
+
 
 }
